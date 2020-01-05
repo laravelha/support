2185eb5b6dc2fe7d5dd1e8d97be3c4d520a25927
Pedro Augusto <pams.pedro@gmail.com>
2020-01-04 21:46:13 -0300

    feat(model): create tableable trait to add behavior of datatable on model
    setting getColumns method on model to render it as a json on blade file

---
+++ src/Traits/Tableable.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Laravelha\Support\Traits;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Yajra\DataTables\Facades\DataTables;
+
+trait Tableable
+{
+    /**
+     * Return array like:
+     * [
+     *     ['data' => 'columnName', 'searchable' => true, 'orderable' => true, 'linkable' => false]
+     * ]
+     * searchable and orderable is true by default
+     * linkable is false by default
+     * @return array[]
+     */
+    abstract public static function getColumns() : array;
+
+    /**
+     * Get datatable
+     *
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public static function getDatatable(): JsonResponse
+    {
+        $columnsCollection = collect(static::getColumns());
+
+        $datas = self::getDatas($columnsCollection);
+
+        $linkables = self::getLinkables($columnsCollection);
+
+        $relations = self::configRelations($datas);
+
+        $list = static::select($datas);
+
+        if(count($relations))
+            $list->with($relations);
+
+        $dataTable = Datatables::of($list);
+
+        foreach ($linkables as $linkable) {
+            $dataTable->editColumn($linkable, function ($item) use ($linkable) {
+                return '<a href="'.route(static::getRouteName().'.show', [static::getObjectName() => $item]).'">'.$item->$linkable.'</a>';
+            });
+        }
+
+        $dataTable->rawColumns($linkables);
+
+        return $dataTable->make(true);
+    }
+
+    /**
+     * Get index data in columns
+     *
+     * @param  Collection  $columnsCollection
+     * @return array
+     */
+    public static function getDatas(Collection $columnsCollection): array
+    {
+        return $columnsCollection->map(function ($column) {
+            return $column['data'] = strpos($column['data'], '.') ? $column['data'] : static::getTableName().'.'.$column['data'];
+        })->toArray();
+    }
+
+    /**
+     * Return likables columns
+     *
+     * @param  Collection  $columnsCollection
+     * @return array
+     */
+    public static function getLinkables(Collection $columnsCollection): array
+    {
+        return $columnsCollection->filter(function ($value) {
+            return $value['linkable'] ?? false;
+        })->pluck('data')->toArray();
+    }
+
+    /**
+     * Set data with relation and return relations
+     *
+     * @param  array  $datas
+     * @return array
+     */
+    public static function configRelations(array &$datas): array
+    {
+        $relations = [];
+        foreach($datas as &$data) {
+            $relation = explode('.', $data);
+
+            if($relation[0] != static::getTableName() && count($relation) > 1) {
+                $relations[] = $relation[0];
+                $data = $relation[0]."_id";
+            }
+        }
+
+        return $relations;
+    }
+
+    /**
+     * Get model name without namespace
+     *
+     * @return string
+     */
+    public static function getModelName(): string
+    {
+        return class_basename(get_called_class());
+    }
+
+    /**
+     * Get table name by the model name
+     *
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return Str::plural(Str::snake(static::getModelName()));
+    }
+
+    /**
+     * Get table name by the model name
+     *
+     * @return string
+     */
+    public static function getRouteName(): string
+    {
+        return Str::kebab(Str::plural(static::getModelName()));
+    }
+
+    /**
+     * Get table name by the model name
+     *
+     * @return string
+     */
+    public static function getObjectName(): string
+    {
+        return Str::snake(static::getModelName());
+    }
+}
